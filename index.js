94529a9ddaff05dd6d55bb35b96562f12ce1b89f
Finn Pauls <derfinn@gmail.com>
2017-12-14 01:40:48 +0100

    update website.json on issue closed

--- index.js
+++ index.js
@@ -19,21 +19,37 @@ module.exports = robot => {
     const labels = issueLabels.data.map(label => label.name)
     if (!labels.includes('next-event')) return
 
+
     context.github.issues.removeLabel(Object.assign(issueRef, {name: 'next-event'}))
     context.github.issues.addLabels(Object.assign(issueRef, {labels: [{name: 'past-event'}]}))
 
-    const nextTitle = getNextTitle(issue.data.title)
-    context.github.issues.create(Object.assign(repoRef, {
+    const oldTitle = issue.data.title
+    const nodeschoolNumber = Number(oldTitle.match(numberRegex)[0].slice(1))
+    const nextNodeschoolNumber = nodeschoolNumber + 1
+    const nextTitle = `NodeSchool #${nextNodeschoolNumber}`
+
+    const nextEventIssue = await context.github.issues.create(Object.assign(repoRef, {
       title: nextTitle,
       body: TODO,
       labels: ['next-event']
     }))
+    removeEventFromWebsite(repoRef, nextEventIssue.data.number, nextNodeschoolNumber)
+
+    async function removeEventFromWebsite  (repoRef, issueNumber, nextNodeschoolNumber) {
+      const path = 'website.json'
+      const res = await context.github.repos.getContent(Object.assign(repoRef, {path}))
+      const {sha, content} = res.data
+      const config = JSON.parse((Buffer.from(content, 'base64')).toString())
+      config.date = false
+      config.number = nextNodeschoolNumber
+      config.issueNumber = issueNumber
+      const base64content = Buffer.from(JSON.stringify(config)).toString('base64')
+      context.github.repos.updateFile(Object.assign(repoRef, {
+        path,
+        sha,
+        content: base64content,
+        message: 'remove event from website',
+      }))
+    }
   })
-}
-
-function getNextTitle (oldTitle) {
-  const nodeschoolNumber = Number(oldTitle.match(numberRegex)[0].slice(1))
-  const nextNodeschoolNumber = nodeschoolNumber + 1
-
-  return `NodeSchool #${nextNodeschoolNumber}`
 }
